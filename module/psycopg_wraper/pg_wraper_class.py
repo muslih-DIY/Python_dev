22c6938196a439e1038680d519c0054eeb4a23be
muslih_k_diy <69723789+muslih-DIY@users.noreply.github.com>
2022-03-28 12:02:08 +0530

    Create pg_wraper_class.py

---
+++ module/psycopg_wraper/pg_wraper_class.py
@@ -0,0 +1,71 @@
+import psycopg2 
+from datetime import datetime
+import cx_Oracle as ora
+import io
+import sys
+
+
+"""
+coder: muslih
+"""
+
+
+class pg2_wrap:
+
+    def __init__(self,connector):
+        self.connector=connector
+        self.con=self.pgconnect(connector)
+        self.command=''
+        self.error=''
+
+    def close(self):
+        self.con.close()
+
+
+    @staticmethod
+    def pgconnect(pgconfig):
+        return psycopg2.connect(
+            user=pgconfig['user'],
+            password=pgconfig['pass'],
+            host=pgconfig['host'],
+            database=pgconfig['database'],
+            port=pgconfig['port'])
+        
+
+    def upd(self,command):
+
+        self.command=command
+        """pgupd will committ if no error found otherwise rollback itself(no need to commit or rollback externally when using as function)"""
+        self.error=''
+        with self.con.cursor() as cur:
+            try:cur.execute(self.command)
+            except Exception as E:
+                self.con.rollback()
+                self.error=str(E)
+                return 0
+            else:
+                self.con.commit()
+                return 1
+    
+
+    def sel(self,command,rtype=None,header=0):
+        self.command=f"select json_agg(t) from ({command}) t" if rtype=='dict' else command      
+        head=None  
+        with self.con.cursor() as cur:
+            try:
+                cur.execute(self.command)
+            except Exception as E:
+                self.error=str(E)
+                return None,0,head    
+            if header and rtype !='dict' :
+                head=[x[0] for x in cur.description]
+            data=cur.fetchall()
+            if rtype=='list':                
+                if len(cur.description)==1:
+                    return [x[0] for x in data],1,head
+                else:
+                    return [list(x) for x in data],1,head
+            if rtype=='dict': return data[0][0] or [],1,head
+            return data,1,head               
+                    
+
