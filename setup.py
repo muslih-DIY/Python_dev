0f7988ef0e06939983acc02d64569b82dc3f3f5c
muslihcode <muslihk00@gmail.com>
2023-01-18 15:47:13 +0530

    setup added

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='dbwraper',
-    version='1.1',   
+    name='dbwraper',  
     description='A simple wraper to work with postgresql and oracle',
     url='https://github.com/muslih-DIY/dbwrapers',
-    author='Stephen Hudson',
+    author='Muslih k',
     author_email='muslihk00@gmail.com',
-    license='BSD 2-clause',
     packages=['dbwraper'],
-    install_requires=[ ],
+
 
     classifiers=[
         'Programming Language :: Python :: 3.5',
